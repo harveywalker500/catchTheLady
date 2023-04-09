39f0641fe3edb756487ecd8a25bcbc7fd063e7d4
decrisp1252 <harveywalker500@gmail.com>
2023-04-09 16:35:03 +0100

    Added gitignore for .vscode

--- src/main.py
+++ src/main.py
@@ -1,16 +1,8 @@
 from card_class_handler import *
 
 
-# Sets up the card deck
-def setup_imports():
-    cardProperties = CardProperties()
-    f_deck = Deck(1, True, cardProperties.value_list, cardProperties.suit_list)
-    return f_deck
-
-
 if __name__ == "__main__":
-    deck = setup_imports()
-    players = []
-    for i in range(5):
-        players.append(Player())
-    print(players)
+    deck = Deck(1, True)
+    print(deck)
+    game_handler = GameHandler(5)
+    print(game_handler)
